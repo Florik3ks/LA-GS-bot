d20f1b9e281895030f0d31a9975b37d2171c3b1e
Florik3ks <floritrs@gmail.com>
2023-05-02 17:30:40 +0200

    wo_tut

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 import asyncio
 import configparser
 from next_tut import get_next_tut
+import random
 
 config = configparser.ConfigParser()
 config.read("secrets.cfg")
@@ -16,8 +17,11 @@ intents.presences = True
 bot = commands.Bot(command_prefix="~", intents=intents, application_id=config['app_id'])
 
 tut_wann = ["wann tut", "tut wann", "wann wieder tut", "tut wieder wann"]
+wann_choices = ["An einem elitären Zeitpunkt", "¯\\_(ツ)_/¯"]
+tut_wo = ["wo tut", "tut wo"]
+wo_choices = ["Mathebau", "20.30 SR 3.061", r"https://www.kit.edu/campusplan/?id=20%2E30&label=20%2E30+SR+%2D1%2E011+%28UG%29+%E2%80%93+Tutorien+Lineare+Algebra+2+f%C3%BCr+Informatik+und+Mathematik"]
 
-def is_tut_msg(message):
+def is_tut_msg(message, options):
     for msg in tut_wann:
         if msg in message.content.lower():
             return True
@@ -36,9 +40,13 @@ async def on_ready():
 async def on_message(message):
     if message.author == bot.user:
         return
-    if is_tut_msg(message):
+    if is_tut_msg(message, tut_wann):
         tut = get_next_tut()
-        await message.channel.send(f"<t:{int(tut[0])}:R> ~ <t:{int(tut[1])}:R>")
+        tut_msg = f"<t:{int(tut[0])}:R> ~ <t:{int(tut[1])}:R>"
+        choices = wann_choices + [tut_msg] * 3
+        await message.channel.send(random.choice(choices))
+    elif is_tut_msg(message, tut_wo):
+        await message.channel.send(random.choice(wo_choices))
 
 
 async def main():
