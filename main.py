d7d0b0bf4b879f88885be9d9fdc48b887751a614
Florik3ks <floritrs@gmail.com>
2023-05-26 17:33:54 +0200

    more w questions

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import discord
-from discord import app_commands
 from discord.ext import commands
 import asyncio
 import configparser
 from next_tut import get_next_tut
 import random
+from collections import OrderedDict
 
 config = configparser.ConfigParser()
 config.read("secrets.cfg")
@@ -16,10 +16,16 @@ intents.message_content = True
 intents.presences = True
 bot = commands.Bot(command_prefix="~", intents=intents, application_id=config['app_id'])
 
-tut_wann = ["wann", "tut"]
-wann_choices = ["An einem elitären Zeitpunkt", "¯\\_(ツ)_/¯"]
-tut_wo = ["wo", "tut"]
-wo_choices = ["Mathebau", "20.30 SR 3.061", r"https://www.kit.edu/campusplan/?id=20%2E30&label=20%2E30+SR+%2D1%2E011+%28UG%29+%E2%80%93+Tutorien+Lineare+Algebra+2+f%C3%BCr+Informatik+und+Mathematik"]
+messages = OrderedDict({
+    "wann": ["An einem elitären Zeitpunkt", "¯\\_(ツ)_/¯"] + [lambda : f"<t:{int(get_next_tut()[0])}:R> ~ <t:{int(get_next_tut()[1])}:R>"] * 3,
+    "warum" : ["Warum nicht?", "trivial. ■"],
+    "wogegen konvergiert": ["???"],
+    "wofür" : ["Für Kühnlein!", "Mathebau"],
+    "wer" : ["Johannes, unser Herr und Meister"],
+    "wieso": ["wieso muss ich das beantworten?", "Für die memes", "µ̴̛̰̖̩̖̪̫͔̥͚͌̄́̎͂̊̆̕̕͜m̷̢͓̩͓̲̝̭̹̪̬̂͊̅͌̋̈́̓̚̚̕ ̵̢̨̧̣̩̳̖͎͈̘̐͛̊́̎͌͑̓͛͠Ļ̵̞̻̗̰̫̺̯̥̉̃̀̍̌̏̄̓̉͘ͅÄ̵̟̖͙̮͕͉̟̹͍͕̈́͗̓̽̓̋̀̔͐͝ ̵̡̦̭̹̖̤̥̪͔̌̌́͌̾͑̑̇̚͝ͅz̸̢̢̭̺̰͓̦̫̙͓͆̉̒͊͂̊̆̊͑̅µ̷̰̭̟̺̳͎͔͇̭̬̇̒́͊͂̽̀̑̏͘ ̷̮̫̲͍̲̜̞͔͖̯̃̽̓̈́̽͛̈́̄̓͝ß̷̤̮̣͍͉̱͕͔̝̝̽̓͆͊̌͗́̃̌͝ề̴̼̩̦̱̱̯̫͚͚̩͋͛͐̈́̚͘̚͝͠§̴̢̨̢̛̛̩̰̱͈̪̹̼̈́̋̏̓͊̃̕͝†̴̨̨̦̖̗̲̼̰̯͖̾̎̈̆̒̒͆͛͗͌ę̸̛̩̬̼̮̹̩̖̫͍̂̋̓́́́͂͋̕͘ḩ̶̨̛̮͈͎̘͎̘͎̰̂̊̇̄̽͒̍̄͊ȩ̴̹̗̝̙͓͉͉̖̣̂̋̿̀̌̽̌͗̆̊͝ñ̴̡̪̮͎̗͇̥̭͇̘̐̀̈̀͆̂̋̏̇͊"],
+    "was": ["https://iwgtfy.com/?q=Was+ist+ein+Tutorium", "dies und das und das Standardskalarprodukt (siehe 11.1.3)"],
+    "wo": ["Mathebau", "20.30 SR 3.061", r"https://www.kit.edu/campusplan/?id=20%2E30&label=20%2E30+SR+%2D1%2E011+%28UG%29+%E2%80%93+Tutorien+Lineare+Algebra+2+f%C3%BCr+Informatik+und+Mathematik"],
+})
 
 def is_tut_msg(message, keywords):
     for key in keywords:
@@ -40,13 +46,15 @@ async def on_ready():
 async def on_message(message):
     if message.author == bot.user:
         return
-    if is_tut_msg(message, tut_wann):
-        tut = get_next_tut()
-        tut_msg = f"<t:{int(tut[0])}:R> ~ <t:{int(tut[1])}:R>"
-        choices = wann_choices + [tut_msg] * 3
-        await message.channel.send(random.choice(choices))
-    elif is_tut_msg(message, tut_wo):
-        await message.channel.send(random.choice(wo_choices))
+    for key in messages.keys():
+        if is_tut_msg(message, key.split()):
+            result = random.choice(messages[key])
+            if type(result) == str:
+                await message.channel.send(result)
+                return
+            else:
+                await message.channel.send(result())
+                return
 
 
 async def main():
