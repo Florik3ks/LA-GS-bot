b8745b94656c4245a3d47c3d8ef49278b53fb896
Florik3ks <floritrs@gmail.com>
2023-05-29 14:58:44 +0200

    fixed timestamp and added time

--- get_assignment_due_date.py
+++ get_assignment_due_date.py
@@ -2,15 +2,16 @@ from PyPDF2 import PdfReader
 import re
 from datetime import datetime
 
-time_pattern = ".*Abgabe[ ]?der[ ]?Lösungen[ ]?bis[ ]?zum[ ]?(\\d\\d.\\d\\d.\\d\\d\\d\\d)[ ]?um.*"
-discord_timestamp = "<t:{timestamp}:D>"
+time_pattern = ".*zum[ ]?(\\d\\d.\\d\\d.\\d\\d\\d\\d)[ ]?um[ ]?(\\d\\d)[ ]?Uhr"
+discord_timestamp = "<t:{timestamp}:D> (<t:{timestamp}:R>)"
 def get_due_date(path):
     pdf_reader = PdfReader(path)
     
     page_1 = pdf_reader.pages[0]
-    x = page_1.extract_text().splitlines()
-    for line in x:
+    lines = page_1.extract_text().splitlines()
+    for line in lines:
         if re.match(time_pattern, line):
             date = re.match(time_pattern, line).group(1)
-            actual_date = datetime.strptime(date, "%d.%m.%Y")
+            time = re.match(time_pattern, line).group(2)
+            actual_date = datetime.strptime(date + " " + time, "%d.%m.%Y %H")
             return discord_timestamp.format(timestamp=int(actual_date.timestamp()))
