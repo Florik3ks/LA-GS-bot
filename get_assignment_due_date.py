44decb89d78c8a844bf89089bb51488a6518e451
Florik3ks <floritrs@gmail.com>
2023-06-30 14:07:19 +0200

    fixed due date not working correctly

--- get_assignment_due_date.py
+++ get_assignment_due_date.py
@@ -6,12 +6,12 @@ time_pattern = ".*zum[ ]?(\\d\\d.\\d\\d.\\d\\d\\d\\d)[ ]?um[ ]?(\\d\\d)[ ]?Uhr"
 discord_timestamp = "<t:{timestamp}:D> (<t:{timestamp}:R>)"
 def get_due_date(path):
     pdf_reader = PdfReader(path)
-    
-    page_1 = pdf_reader.pages[0]
-    lines = page_1.extract_text().splitlines()
-    for line in lines:
-        if re.match(time_pattern, line):
-            date = re.match(time_pattern, line).group(1)
-            time = re.match(time_pattern, line).group(2)
-            actual_date = datetime.strptime(date + " " + time, "%d.%m.%Y %H")
-            return discord_timestamp.format(timestamp=int(actual_date.timestamp()))
+    for page in pdf_reader.pages:
+        lines = page.extract_text().splitlines()
+        for line in lines:
+            if re.match(time_pattern, line):
+                date = re.match(time_pattern, line).group(1)
+                time = re.match(time_pattern, line).group(2)
+                actual_date = datetime.strptime(date + " " + time, "%d.%m.%Y %H")
+                return discord_timestamp.format(timestamp=int(actual_date.timestamp()))
+    return "keine Ahnung, da kein Datum gefunden"
